f9907d0fa3bbcd7c4d0fec6e89dd9d2e6bb4517f
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2022-10-21 17:01:32 +0200

    update

--- libft/ft_str_is_num.c
+++ libft/ft_str_is_num.c
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/21 16:05:43 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/21 16:13:52 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/21 16:58:59 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	ft_str_is_num(char *str)
 	size_t	counter;
 
 	counter = 0;
-	while (str[counter])
-	{
-		if (!ft_isdigit(str[counter]))
-			return (0);
+	if (!str)
+		return (0);
+	if (str[0] == '-' || str[0] == '+')
 		counter++;
-	}
+	while (ft_isdigit(str[counter]))
+		counter++;
+	if (str[counter])
+		return (0);
 	return (1);
 }
